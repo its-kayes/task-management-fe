c035305c055aeb19795affc397a777fd06f2aa03
Kayes <kayes.ek8@gmail.com>
2023-01-13 15:20:27 +0600

    Side Navbar Implement

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Loader from "./shared/Loader";
 import { lazy } from "react";
+import SideNavbar from "./shared/nav/SideNavbar";
+import Test from "./pages/Test";
 
-const Home = lazy(() => import("./components/Home/Home"));
+const Home = lazy(() => import("./pages/Home"));
 
 function App() {
   return (
     <Suspense fallback={<Loader />}>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<SideNavbar />}>
+          <Route index element={<Home />} />
+          <Route path="test" element={<Test />} />
+        </Route>
       </Routes>
     </Suspense>
   );
