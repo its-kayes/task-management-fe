9b51bc8c045b522cabace19814f7a1692257c497
Kayes <kayes.ek8@gmail.com>
2023-01-16 03:17:12 +0600

    update design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,17 @@ import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
 import { useState } from "react";
-import { addEventSuccess, storeEvents } from "../store/events/eventsSlice";
+// import { addEventSuccess, storeEvents } from "../store/events/eventsSlice";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import List from "@mui/material/List";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -47,18 +57,102 @@ export default function Home() {
     // dispatch(storeEvents(events));
   };
 
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
   return (
-    <div className="">
-      <section className="w-2/3 ">
-        <FullCalendar
-          initialView="dayGridMonth"
-          plugins={[dayGridPlugin, interactionPlugin]}
-          // weekends={false}
-          events={events}
-          dateClick={handleDateClick}
-          eventsSet={handleEvents}
-        />
-      </section>
-    </div>
+    <Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={6} md={8}>
+            <Item
+              sx={
+                {
+                  // maxHeight: 900,
+                }
+              }
+            >
+              <FullCalendar
+                height={650}
+                initialView="dayGridMonth"
+                plugins={[dayGridPlugin, interactionPlugin]}
+                // weekends={false}
+                events={events}
+                dateClick={handleDateClick}
+                eventsSet={handleEvents}
+              />
+            </Item>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <Item>
+              <List
+                sx={{
+                  width: "100%",
+                  // maxWidth: 360,
+                  bgcolor: "background.paper",
+                  position: "relative",
+                  overflow: "auto",
+                  maxHeight: 650,
+                  "& ul": { padding: 0 },
+                }}
+                subheader={<li />}
+              >
+                {[1, 2, 3, 4, 5, 6].map((cardItem) => (
+                  <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                      <Typography
+                        sx={{ fontSize: 14 }}
+                        color="text.secondary"
+                        gutterBottom
+                      >
+                        Word of the Day
+                      </Typography>
+                      <Typography variant="h5" component="div">
+                        check
+                      </Typography>
+                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        adjective
+                      </Typography>
+                      <Typography variant="body2">
+                        well meaning and kindly.
+                        <br />
+                        {'"a benevolent smile"'}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Learn More</Button>
+                    </CardActions>
+                  </Card>
+                ))}
+              </List>
+            </Item>
+          </Grid>
+          <Grid item xs={6} md={8}>
+            <Item>xs=6 md=8</Item>
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <Item>xs=6 md=4</Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
   );
 }
+
+// <div className="">
+//   <section className="w-2/3 ">
+//     <FullCalendar
+//       initialView="dayGridMonth"
+//       plugins={[dayGridPlugin, interactionPlugin]}
+//       // weekends={false}
+//       events={events}
+//       dateClick={handleDateClick}
+//       eventsSet={handleEvents}
+//     />
+//   </section>
+// </div>
